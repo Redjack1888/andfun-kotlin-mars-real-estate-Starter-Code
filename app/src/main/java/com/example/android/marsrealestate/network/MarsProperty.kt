98dcbd702dc6761cac4019ac016778c739b2af1c
Alessandro <alessandro.mana@gmail.com>
2021-11-16 02:01:11 +0100

    Added Parcelize Annotation to MarsProperty.kt and transformed the object in a Parcelable one.

--- app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -17,10 +17,14 @@
 
 package com.example.android.marsrealestate.network
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.parcelize.Parcelize
 
-class MarsProperty(
+@Parcelize
+data class MarsProperty(
     val id: String,
+    // used to map img_src from the JSON to imgSrcUrl in our class
     @Json(name = "img_src") val imgSrcUrl: String,
     val type: String,
-    val price: Double)
+    val price: Double) : Parcelable
